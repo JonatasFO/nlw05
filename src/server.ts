9a5afa26fb2730c91f1bf648bf5bc992878d3ca8
Jonatas <jnsfoliveira@gmail.com>
2021-04-20 01:14:57 -0300

    configuracao inicial | criando rota get, post

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({message: "Olá NLW 05 🚀️"})
+})
+
+app.post("/users", (request, response) => {
+  return response.json({message: "Usuário salvo com sucesso!"})
+})
+
+app.listen(3333,
+    () => console.log("Server is running on port 3333")
+  );
