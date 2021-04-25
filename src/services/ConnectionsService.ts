9d0bb1f769159944700f7c2036261e72f4801bee
Jonatas <jnsfoliveira@gmail.com>
2021-04-24 22:55:26 -0300

    websocket configured

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -12,34 +12,61 @@ interface IConnectionCreate {
 }
 
 class ConnectionsService {
-
+  
   private connectionsRepository: Repository<Connection>
-
+  
   constructor() {
     this.connectionsRepository = getCustomRepository(ConnectionsRepository)
   }
-
+  
   async create({ socket_id, user_id, admin_id, id } : IConnectionCreate) {
-
+    
     const connection = this.connectionsRepository.create({
       socket_id,
       user_id,
       admin_id,
       id
     })
-
+    
     await this.connectionsRepository.save(connection);
-
+    
     return connection;
-
+    
   }
-
+  
   async findByUserId(user_id: string) {
     const connection = await this.connectionsRepository.findOne({user_id});
-
+    
     return connection;
   }
+  
+  async findAllWithoutService() {
+    
+    const connections = await this.connectionsRepository.find({
+      where: {admin_id: null},
+      relations: ["user"],
+    });
+    
+    return connections;
+    
+  }
 
+  async findBySocketId(socket_id: string) {
+
+    const connection = await this.connectionsRepository.findOne({socket_id});
+
+    return connection;
+
+  }
+
+  async updateAdminId(user_id: string, admin_id: string){
+    await this.connectionsRepository.createQueryBuilder()
+    .update(Connection)
+    .set({ admin_id })
+    .where("user_id = :user_id", { user_id })
+    .execute();
+  }
+  
 }
 
 export { ConnectionsService }
